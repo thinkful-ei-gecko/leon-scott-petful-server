380a11ce13bfc2c0c57b958bff3db8edadf8751a
Scott Schroeder <snschroeder89@gmail.com>
2019-11-16 16:35:41 -0600

    heroku setup

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-client.ldail.now.sh/',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
 };
